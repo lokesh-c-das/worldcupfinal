f7eb574d510b4b4f723b6e72a9fa821350f9f110
Lokesh Chandra Das <ldas@memphis.edu>
2020-02-24 15:42:41 -0600

    Dummy data has been added

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+WorldCupFinal.create!([{
+    year: 1998, champion: 'Spain', champion_score: 1, runner_up: 'France', runner_up_score: 0}, {year: 2002, champion: 'Italy', champion_score: 1, runner_up: 'France', runner_up_score: 0},{year: 2006, champion: 'United Arab Emirates', champion_score: 3, runner_up: 'Germany', runner_up_score: 2}]
+)
