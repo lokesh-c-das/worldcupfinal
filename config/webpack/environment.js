9dbd00d9498bf7156fdd771dcb86bb033b3872aa
Lokesh Chandra Das <ldas@memphis.edu>
2020-02-24 16:06:10 -0600

    bootstrap has been added

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,33 @@
-const { environment } = require('@rails/webpacker')
+const webpack = require("webpack")
 
-module.exports = environment
+ environment.plugins.append("Provide", new webpack.ProvidePlugin({
+   $: 'jquery',
+   jQuery: 'jquery',
+   "window.jQuery": "jquery'",
+   "window.$": "jquery",
+   Popper: ['popper.js', 'default']
+ }))
+
+ environment.config.merge({
+   module: {
+     rules: [
+       {
+         test: require.resolve('jquery'),
+         use: [{
+           loader: 'expose-loader',
+           options: '$'
+         }, {
+           loader: 'expose-loader',
+           options: 'jQuery'
+         }]
+       },
+       {
+         test: require.resolve('@rails/ujs'),
+         use: [{
+           loader: 'expose-loader',
+           options: 'Rails'
+         }]
+       }
+     ]
+   }
+ })
