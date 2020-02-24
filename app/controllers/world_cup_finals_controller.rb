a2c9f3bfd16aa06b8f49ddc1350aa07e37fb3579
Lokesh Chandra Das <ldas@memphis.edu>
2020-02-24 17:04:54 -0600

    Edit and delete option has been added

--- app/controllers/world_cup_finals_controller.rb
+++ app/controllers/world_cup_finals_controller.rb
@@ -34,4 +34,37 @@ class WorldCupFinalsController < ApplicationController
       end
     end
   end
+
+  def edit
+    world_cup_final = WorldCupFinal.find(params[:id])
+    respond_to do |format|
+      format.html{render :edit, locals:{world_cup_final:world_cup_final}}
+    end
+  end
+  def update
+    wc_final = WorldCupFinal.find(params[:id])
+    respond_to do |format|
+      format.html do 
+        if wc_final.update(params.require(:world_cup_final).permit(:year,:champion, :champion_score, :runner_up, :runner_up_score))
+          flash[:success] = "New World cup updated successfully!"
+          redirect_to world_cup_finals_url
+        else
+          flash.now[:danger] = "Error: World cup could not be updated"
+          render :new, locals:{world_cup_final:wc_final}
+        end
+      end
+    end
+  end
+  def destroy
+    world_cup_final = WorldCupFinal.find(params[:id])
+    world_cup_final.destroy
+    respond_to do |format|
+      format.html do
+        # success message
+        flash[:success] = 'Question removed successfully'
+        # redirect to index
+        redirect_to world_cup_finals_url
+      end
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,13 @@ Rails.application.routes.draw do
   get 'world_cup_finals/:id', to:'world_cup_finals#show', as:'world_cup_final'
 
   post 'world_cup_finals', to:'world_cup_finals#create' #create
+
+
+  #edit 
+  get 'world_cup_finals/:id/edit', to:'world_cup_finals#edit', as:'edit_world_cup_final'
+  patch 'world_cup_finals/:id', to:'world_cup_finals#update' #update
+  put 'world_cup_finals/:id', to:'world_cup_finals#update'
+
+  delete 'world_cup_finals/:id', to:'world_cup_finals#destroy'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
