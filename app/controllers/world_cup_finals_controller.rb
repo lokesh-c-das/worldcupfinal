a943d621ecd7e2deabad4024123a4454bee62f43
Lokesh Chandra Das <ldas@memphis.edu>
2020-02-24 16:12:38 -0600

    show page has been added:

--- app/controllers/world_cup_finals_controller.rb
+++ app/controllers/world_cup_finals_controller.rb
@@ -5,4 +5,11 @@ class WorldCupFinalsController < ApplicationController
       format.html{render :index, locals:{finals:finals}}
     end
   end
+
+  def show
+    world_cup_final = WorldCupFinal.find(params[:id])
+    respond_to do |format|
+      format.html{render :show, locals:{world_cup_final:world_cup_final}}
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,9 @@ Rails.application.routes.draw do
   
   root to: 'world_cup_finals#index'
   get 'world_cup_finals', to:'world_cup_finals#index', as:'world_cup_finals'
+
+  #create 
+  get 'world_cup_finals/new', to:'world_cup_finals#create', as:'new_world_cup_final'
+  get 'world_cup_finals/:id', to:'world_cup_finals#show', as:'world_cup_final'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
